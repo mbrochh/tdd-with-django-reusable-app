5a2bd7324d066f12c8a20f286ee462454c3ad838
Martin Brochhaus <mbrochh@gmail.com>
2012-06-08 22:08:30 +0800

    Commenting out the bad code

--- myapp2/views.py
+++ myapp2/views.py
@@ -1,12 +1,12 @@
 """Views for appname application."""
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+# from django.contrib.auth.decorators import login_required
+# from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
     template_name = 'myapp2/home.html'
 
-    @method_decorator(login_required)
+    # @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(HomeView, self).dispatch(*args, **kwargs)
