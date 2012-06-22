9af618c63bdebeb31ffafb5c79787a6fec3f8949
Martin Brochhaus <mbrochh@gmail.com>
2012-06-22 22:48:48 +0800

    Added info on how to publish the app on pypi.python.org

--- setup.py
+++ setup.py
@@ -6,6 +6,23 @@ that your potential users can easily install it via ``easy_install`` or ``pip`.
 However, you might never need to use this since you can just host your app
 on github or bitbucket and use ``pip`` to install it from there.
 
+In order to test this package with an existing Django project, activate that
+project's virtualenv and run::
+
+    python setup.py develop
+
+This will build the app in the same folder and just add a reference to your
+virtualenv's easy-install.pth file.
+
+When you are ready for a release you can register your desired package name and
+upload yout work.  You need an account at pypi.python.org for this::
+
+    python setup.py register
+    python setup.py sdist upload
+
+For more information please see this guide:
+http://guide.python-distribute.org/quickstart.html
+
 """
 import os
 from setuptools import setup, find_packages
